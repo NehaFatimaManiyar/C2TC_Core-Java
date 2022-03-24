df8c81670bec2a6389eae5f4d009129a1a37bd97
Admin <Admin@DESKTOP-FG43D12>
2022-03-25 02:15:10 +0530

    first commit

---
+++ PracticeSet/src/com/operation/p1/Helloworld.java
@@ -0,0 +1,10 @@
+package com.operation.p1;
+
+public class Helloworld {
+	String s;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello ");
+	}
+
+}
