e93bf08472d6193f695fdd9948fabacdf3175b1e
Admin <Admin@DESKTOP-FG43D12>
2022-04-02 13:09:55 +0530

    first commit

---
+++ PracticeSet/src/newprogram/LadderElse_If.java
@@ -0,0 +1,17 @@
+package newprogram;
+
+public class LadderElse_If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      char city = 'P';
+      if(city == 'B') 
+    	  System.out.println("City is Bombay.");
+    	  else if(city == 'N')
+    		  System.out.println("City is Nagpur.");
+    	  else if (city == 'P')
+    		  System.out.println("City is Pune.");
+    	  }
+      
+	}
+
