e9f28e62ca17883d6549e86ad714c34e0c1935c6
Admin <Admin@DESKTOP-FG43D12>
2022-04-03 13:16:58 +0530

    commit first

---
+++ AppletDemo1/src/com/applet/AppletProg1.java
@@ -0,0 +1,29 @@
+package com.applet;
+
+	import java.applet.Applet;
+	import java.awt.Button;
+	import java.awt.Event;
+	import java.awt.TextField;
+
+
+	public class AppletProg1  extends Applet{
+		Button b1,b2;
+		TextField t; 
+		public void init() {
+			//b1 = new Button("Click");
+			//t = new TextField(5);
+			//b2 = new Button("Clear");
+			add(b1 = new Button("Click"));
+			add(t= new TextField(5));
+			add(b2= new Button("Clear"));
+		}
+		public boolean action(Event e, Object o) {
+			if(e.target.equals(b1))
+			 t.setText("Hello");
+			if(e.target.equals(b2))
+				 t.setText("");
+			return true;
+			
+		}
+
+}
