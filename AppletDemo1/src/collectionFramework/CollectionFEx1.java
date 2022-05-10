f986091bc67586f98b96b9e41e29856240b7d524
Admin <Admin@DESKTOP-FG43D12>
2022-05-10 08:32:24 +0530

    first commit

---
+++ AppletDemo1/src/collectionFramework/CollectionFEx1.java
@@ -0,0 +1,19 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+//The Collections framework is defined in the java.util package
+
+public class CollectionFEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> animals = new ArrayList<>();
+        // Add elements
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList: " + animals);
+	}
+
+}
